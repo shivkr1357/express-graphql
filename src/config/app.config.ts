4ee5346d9deb231854c1cf152af4040eda8c20eb
ShivPrasad <shivshankar.pusa@gmail.com>
2024-02-29 13:02:50 +0530

    Change in picture upload api

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -5,71 +5,71 @@ type DestinationCallback = (error: Error | null, destination: string) => void;
 type FileNameCallback = (error: Error | null, filename: string) => void;
 
 export const appConfig = {
-   DB_URL: "mongodb://0.0.0.0:27017/express-graphql",
-   DB_USERNAME: "",
-   DB_PASSWORD: "",
-   ACCESS_TOKEN_PRIVATE_KEY: "newmoduleexpress",
-   REFRESH_TOKEN_PRIVATE_KEY: "shivshankarkumar.pusa",
-   SALT: 10,
+  DB_URL: "mongodb://0.0.0.0:27017/express-graphql",
+  DB_USERNAME: "",
+  DB_PASSWORD: "",
+  ACCESS_TOKEN_PRIVATE_KEY: "newmoduleexpress",
+  REFRESH_TOKEN_PRIVATE_KEY: "shivshankarkumar.pusa",
+  SALT: 10,
 };
 
 export const swaggerOptions = {
-   definition: {
-      openapi: "3.1.0",
-      info: {
-         title: "New APIs for social media",
-         version: "0.1.0",
-         description:
-            "This is the backend API of the SocialMedia application, documented using Swagger",
-         license: {
-            name: "MIT",
-            url: "https://spdx.org/licenses/MIT.html",
-         },
-         contact: {
-            name: "ItsIndianGuy",
-            url: "https://itsindianguy.in",
-            email: "shivshankarkumar.pusa@gail.com",
-         },
+  definition: {
+    openapi: "3.1.0",
+    info: {
+      title: "New APIs for social media",
+      version: "0.1.0",
+      description:
+        "This is the backend API of the SocialMedia application, documented using Swagger",
+      license: {
+        name: "MIT",
+        url: "https://spdx.org/licenses/MIT.html",
       },
-      servers: [
-         {
-            url: "http://localhost:4000/api/v1",
-         },
-      ],
-   },
-   apis: ["./src/routes/*.ts"], // Specify the main index file that includes all routes
+      contact: {
+        name: "ItsIndianGuy",
+        url: "https://itsindianguy.in",
+        email: "shivshankarkumar.pusa@gail.com",
+      },
+    },
+    servers: [
+      {
+        url: "http://localhost:4000/api/v1",
+      },
+    ],
+  },
+  apis: ["./src/routes/*.ts"], // Specify the main index file that includes all routes
 };
 
 export const fileStorage = multer.diskStorage({
-   destination: (
-      request: Request,
-      file: Express.Multer.File,
-      callback: DestinationCallback
-   ): void => {
-      // ...Do your stuff here.
-   },
+  destination: (
+    request: Request,
+    file: Express.Multer.File,
+    callback: DestinationCallback
+  ): void => {
+    // ...Do your stuff here.
+  },
 
-   filename: (
-      req: Request,
-      file: Express.Multer.File,
-      callback: FileNameCallback
-   ): void => {
-      // ...Do your stuff here.
-   },
+  filename: (
+    req: Request,
+    file: Express.Multer.File,
+    callback: FileNameCallback
+  ): void => {
+    // ...Do your stuff here.
+  },
 });
 
 export const fileFilter = (
-   request: Request,
-   file: Express.Multer.File,
-   callback: FileFilterCallback
+  request: Request,
+  file: Express.Multer.File,
+  callback: FileFilterCallback
 ): void => {
-   if (
-      file.mimetype === "image/png" ||
-      file.mimetype === "image/jpg" ||
-      file.mimetype === "image/jpeg"
-   ) {
-      callback(null, true);
-   } else {
-      callback(null, false);
-   }
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    callback(null, true);
+  } else {
+    callback(null, false);
+  }
 };
--- src/server.ts
+++ src/server.ts
@@ -9,6 +9,8 @@ import routes from "./routes/index"; // Import the main index file that includes
 import { appConfig, swaggerOptions } from "./config/app.config";
 import multer from "multer";
 import { fileFilter, storage } from "./utils/multer";
+import path from "path";
+import fs from "fs";
 
 const app = express();
 dotenv.config();
@@ -31,27 +33,71 @@ app.use("/api/v1/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
 // make a separate upload routes for the images upload
 
+// Endpoint to handle image uploads
 app.post("/api/v1/upload", upload.single("image"), (req, res) => {
-   res.status(200).json({ message: "Image uploaded successfully" });
+  try {
+    const { type, postId } = req.body;
+
+    if (!type) {
+      return res.status(400).json({ error: "Type and postId are required" });
+    }
+
+    const allowedTypes = ["post", "user"];
+    if (!allowedTypes.includes(type)) {
+      return res.status(400).json({ error: "Invalid type" });
+    }
+
+    const image = req.file;
+
+    if (!image) {
+      return res.status(400).json({ error: "No image uploaded" });
+    }
+
+    const uploadPath = `uploads/${type}`;
+
+    // Check if the directory exists, if not create it
+    let date = new Date();
+
+    fs.mkdirSync(uploadPath, { recursive: true });
+    const extname = path.extname(image.originalname);
+
+    // Generate new filename with lowercase original name and timestamp
+    const filename = `${path
+      .basename(image.originalname, extname)
+      .toLowerCase()
+      .replace(/\s+/g, "")}_${Date.now()}${extname}`;
+
+    console.log("File naame", filename);
+
+    // Move the uploaded file to the appropriate folder
+    fs.renameSync(image.path, path.join(uploadPath, filename));
+
+    res.status(200).json({ message: "Image uploaded successfully" });
+  } catch (error) {
+    console.log("Error", error);
+    return res
+      .status(500)
+      .json({ error: true, message: "Internal server error" });
+  }
 });
 
 // Default route
 app.use("*", (req, res) => {
-   res.send("Hello from express server");
+  res.send("Hello from express server");
 });
 
 // Log the discovered routes
-console.log("Discovered routes:", Object.keys(specs?.paths));
+// console.log("Discovered routes:", Object.keys(specs?.paths));
 
 // MongoDB connection and Server start
 mongoose
-   .connect(appConfig.DB_URL)
-   .then(() => {
-      console.log("MongoDB connected to the database");
-      app.listen(port, () => {
-         console.log(`App listening on port ${port}`);
-      });
-   })
-   .catch((error) => {
-      console.error("Error connecting to MongoDB:", error);
-   });
+  .connect(appConfig.DB_URL)
+  .then(() => {
+    console.log("MongoDB connected to the database");
+    app.listen(port, () => {
+      console.log(`App listening on port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
